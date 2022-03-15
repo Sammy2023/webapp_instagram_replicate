6f9589107bec4c1be46f51dcf507c9d37155f1b8
Glitch (glitch-hello-react) <none>
2022-03-15 03:05:01 +0000

    🌈✊ Checkpoint ./src/components/Navbar.jsx:41223157/426

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import css from './Navbar.module.css';
 
 export default function Navbar(){
   function handleClick(page){
     
   }
   
-  return (<nav>
-    <div>
+  return (
+  <nav>
+    <div className={css.navbar}>
+      <button>
+        <img src="https://cdn.glitch.global/9bff44da-05b9-4d83-b4f6-75df2a433bfe/home.svg?v=1647286968967" alt="Home"/>
+      </button>
       Navbar
     </div>
   </nav>
