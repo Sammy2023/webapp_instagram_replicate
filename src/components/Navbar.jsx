a73b6aaf0e1e2cf4a4c00bb1119eb4c32adb2bdf
Glitch (glitch-hello-react) <none>
2022-03-20 22:04:27 +0000

    🌠🏔 Checkpoint ./src/components/Navbar.jsx:41223157/339

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,22 +14,22 @@ export default function Navbar(){
       </button>
     </div>
     <div className={css.navItem}>
-      <button>
+      <button onClick={e=>handleNavChange('explore')}>
         <img src="https://cdn.glitch.global/9bff44da-05b9-4d83-b4f6-75df2a433bfe/explore.svg?v=1647286962718"/>
       </button>
     </div>
     <div className={css.navItem}>
-      <button>
+      <button> onClick={e=>handleNavChange('newpost')}
         <img src="https://cdn.glitch.global/9bff44da-05b9-4d83-b4f6-75df2a433bfe/newpost.svg?v=1647287005893"/>
       </button>
     </div>
     <div className={css.navItem}>
-      <button>
+      <button onClick={e=>handleNavChange('like')}>
         <img src="https://cdn.glitch.global/9bff44da-05b9-4d83-b4f6-75df2a433bfe/like.svg?v=1647287005893"/>
       </button>
     </div>
     <div className={css.navItem}>
-      <button>
+      <button onClick={e=>handleNavChange('profile')}>
         <img src="https://cdn.glitch.global/9bff44da-05b9-4d83-b4f6-75df2a433bfe/profile.svg?v=1647287005893"/>
       </button>
     </div>
