65b60ad992bb6989cbe73eebea3b19cd454df943
Glitch (glitch-hello-react) <none>
2022-03-28 20:00:41 +0000

    👢💵 Checkpoint ./src/components/Home.jsx:41223157/1082

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,41 +2,48 @@ import React from 'react';
 import Post from './Post.jsx'
 import css from "../styles/Home.module.css";
 
-function Home() {
-  const post ={
-    user:{
-      id:"judy",
-      photo:"https://cdn.glitch.global/9bff44da-05b9-4d83-b4f6-75df2a433bfe/user1.png?v=1647287055846",
-    },
-    post:{
-      id:"post-1",
-      userId:"judy",
-      photo:"https://cdn.glitch.global/9bff44da-05b9-4d83-b4f6-75df2a433bfe/post1.png?v=1647287012210",
-      desc:"#zootopia #excited",
-      datetime: "2020-02-09T22:45:28Z"
-    },
-    likes: {
-      self: true,
-      count:1
-    },
-    comments:[
-      {
-        userId:"nick",
-        text:"Welcome to Zootopia!"
-      },
-      {
-        userId:"judy",
-        text:"Thanks!😁Looking forward to meeting you!"
-      }
-    ]
-  };
+function Home(props) {
+  // const post ={
+  //   user:{
+  //     id:"judy",
+  //     photo:"https://cdn.glitch.global/9bff44da-05b9-4d83-b4f6-75df2a433bfe/user1.png?v=1647287055846",
+  //   },
+  //   post:{
+  //     id:"post-1",
+  //     userId:"judy",
+  //     photo:"https://cdn.glitch.global/9bff44da-05b9-4d83-b4f6-75df2a433bfe/post1.png?v=1647287012210",
+  //     desc:"#zootopia #excited",
+  //     datetime: "2020-02-09T22:45:28Z"
+  //   },
+  //   likes: {
+  //     self: true,
+  //     count:1
+  //   },
+  //   comments:[
+  //     {
+  //       userId:"nick",
+  //       text:"Welcome to Zootopia!"
+  //     },
+  //     {
+  //       userId:"judy",
+  //       text:"Thanks!😁Looking forward to meeting you!"
+  //     }
+  //   ]
+  // };
+  const {posts, users, likes, comments} = props;
   
   return (
     <div className={css.homeContainer}>
-      <Post user={post.user} 
-            likes = {post.likes} 
-            post = {post.post} 
-            comments={post.comments}  />
+      {posts.sort((a,b)=>new Date(b.datetime) - new Date(a.datetime))
+       .map(post =>
+           <Post 
+             key= {post.id}
+             user={post.user} 
+             likes = {post.likes} 
+             post = {post.post} 
+             comments={post.comments}  />
+      )}
+     
     </div>
   );
 }
